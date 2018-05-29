1c125c81520c01233b963ec7c5737d29093faef3
ehoversten <ehoversten@gmail.com>
2018-05-29 18:09:30 -0400

    updated file

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,30 +2,34 @@ function bubbleSort(arr) {
   // set current to an initial value;
   var current = arr[0];
   var length = arr.length;
-  console.log(length);
+  console.log('Array contains: ' + length);
   var next;
   var previous;
-  for(var i = 0; i < (arr.length-1)-1; i++) {
-    console.log("i is: " + i);
-    console.log("comparing " + arr[i] + " and " + arr[i+1] +"\n");
+  for(var j = 0; j <= arr.length-1; j++) {
+    for(var i = 0; i <= (arr.length-1)-1; i++) {
+      console.log("index is: " + i);
+      console.log("comparing " + arr[i] + " and " + arr[i+1] +"\n");
 
-    if(arr[i] > arr[i+1]) {
-      console.log("value at " + arr[i] + " is greater than " + arr[i+1] +"\n");
-      var temp = arr[i];
-      arr[i] = arr[i+1];
-      arr[i+1] = temp;
-      console.log(arr);
-    } else if (arr[i] < arr[i+1]) {
-      console.log("value at " + arr[i] + " is less than " + arr[i+1] +"\n");
-      console.log(arr);
+      if(arr[i] > arr[i+1]) {
+        console.log("value at " + arr[i] + " is greater than " + arr[i+1] +"\n");
+        var temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        console.log(arr);
+      } else if (arr[i] < arr[i+1]) {
+        console.log("value at " + arr[i] + " is less than " + arr[i+1] +"\n");
+        console.log(arr);
 
-      continue;
-    } else {
-      console.log("value at " + arr[i] + " is equal to " + arr[i+1] +"\n");
-      console.log(arr);
+        continue;
+      } else {
+        console.log("value at " + arr[i] + " is equal to " + arr[i+1] +"\n");
+        console.log(arr);
 
-      continue;
-    }
-    console.log()
-  }
+        continue;
+      }
+      console.log('comparing next set of values\n')
+    } // end of internal for loop
+
+  } // end of external for loop
+  return arr;
 }
